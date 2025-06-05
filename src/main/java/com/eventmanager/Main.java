01f45c0debfcbb5b18545e48484363e2d1cbf32b
Luana Lima <luanalima.2006@hotmail.com>
2025-06-05 10:28:52 +0000

    fix: adapt codespaces to jdk 24

--- src/main/java/com/eventmanager/Main.java
+++ src/main/java/com/eventmanager/Main.java
@@ -2,6 +2,6 @@ package com.eventmanager;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Rodando com sucesso!");
+        System.out.println("Roda namoralzinha");
     }
 }
